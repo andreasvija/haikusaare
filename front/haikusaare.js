6962689be35feeb2dd53e0ef3b3cb5579e2e49e7
andreasvija <andreasvija@hotmail.com>
2020-02-24 22:46:10 +0200

    haiku loading clarity

--- front/haikusaare.js
+++ front/haikusaare.js
@@ -2,10 +2,14 @@ const inspirations = ["talu", "mees", "perenaine", "kala", "mets", "hobune", "ae
 function setRandomInsp() {
     const insp = inspirations[Math.floor(Math.random() * inspirations.length)];
     document.getElementById("insp").value = insp;
-};
+}
 
 function sendHaikuRequest() {
-    var insp = document.getElementById("insp").value;
+    document.getElementById("haiku1").innerHTML = "_____";
+    document.getElementById("haiku2").innerHTML = "_______";
+    document.getElementById("haiku3").innerHTML = "_____";
+
+    let insp = document.getElementById("insp").value;
     if (insp.length > 200) {
         insp = "";
     }
@@ -13,8 +17,8 @@ function sendHaikuRequest() {
 
     const request = new XMLHttpRequest();
     request.onreadystatechange = function () {
-        if (this.readyState == 4) {
-            if (this.status == 200) {
+        if (this.readyState === 4) {
+            if (this.status === 200) {
                 const lines = this.responseText.split("\n");
                 document.getElementById("haiku1").innerHTML = lines[0];
                 document.getElementById("haiku2").innerHTML = lines[1];
@@ -27,7 +31,7 @@ function sendHaikuRequest() {
     };
     request.open("GET", "/haikusaare/haiku?insp="+insp, true);
     request.send();
-};
+}
 
 window.addEventListener("DOMContentLoaded", function() {
     setRandomInsp();
